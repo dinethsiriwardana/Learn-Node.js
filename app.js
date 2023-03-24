f1e60997b41e4a3f5af36ea041111e97f9bdaf51
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-24 07:54:31 +0530

    Pipes and Pipe Chaining

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-// const fs = require('fs');
-// const zlib = require('zlib');
+const fs = require("fs");
+const zlib = require("zlib"); //? for compressing and decompressing files
 
-// const readStream = fs.createReadStream('./example.txt', 'utf8');
-// const writeStream = fs.createWriteStream('example2.txt');
-// readStream.pipe(writeStream);
+// const gzip = zlib.createGzip(); //? create a gzip object, gzip for compressing
+
+// const readStream = fs.createReadStream("./bigfile.txt", "utf8");
+// const writeStream = fs.createWriteStream("bigfile2.txt.gz");
+
+// readStream.pipe(gzip).pipe(writeStream); //? pipe the readStream to the gzip object compress the file
+
+const gunzip = zlib.createGunzip(); //? create a gunzip object, gunzip for decompressing
+
+const readStream = fs.createReadStream("./bigfile2.txt.gz");
+const writeStream = fs.createWriteStream("extract.txt");
+
+readStream.pipe(gunzip).pipe(writeStream); //? pipe the readStream to the gunzip object extract the file
