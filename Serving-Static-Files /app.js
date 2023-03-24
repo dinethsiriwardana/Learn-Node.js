71881b194993df141ad06ad02267f438d2f14e2a
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-24 09:23:23 +0530

    Serving Static Files

---
+++ Serving-Static-Files /app.js
@@ -0,0 +1,15 @@
+const http = require("http"); //? Importing the http module
+
+const fs = require("fs"); //? Importing the fs module
+
+http.createServer((req, res) => {
+ 
+//    const readStream = fs.createReadStream("./index.html");
+//     res.writeHead(200, { "Content-type": "text/html" }); //? 200 is the status code for OK
+//    const readStream = fs.createReadStream("./example.json");
+//     res.writeHead(200, { "Content-type": "application/json" }); //? 200 is the status code for OK
+   const readStream = fs.createReadStream("./image.jpg");
+    res.writeHead(200, { "Content-type": "image/png" }); //? 200 is the status code for OK
+    readStream.pipe(res);
+
+}).listen(3001);
