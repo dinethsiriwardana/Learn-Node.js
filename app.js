55517c1a2f9d4f011dedb10617eeae791563d3f3
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-26 08:43:27 +0530

    Working with JSON Data with Express and the Body Parser Module

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'Static'))); //? static files
 app.use(body.urlencoded({extended: false})); //? body parser
+app.use(body.json()); //? body parser json data
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'Static', 'index.html')); //? sending a file to the browser
@@ -13,7 +15,9 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     console.log(req.body);
     //? database work here
-    res.send('successfully posted data');
+    // res.send('successfully posted data');
+    res.json({success: true}); //? sending json data to the browser
     });
 
 app.listen(3001);
+ 
