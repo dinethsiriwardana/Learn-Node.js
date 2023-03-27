1df1f9958b859f7a90be0f0b0041078e31f6f381
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-27 22:35:52 +0530

    EJS Templates With Express

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const express = require("express");
 const path = require("path");
-const Joi = require("joi"); //? joi validation
-const body = require("body-parser"); //? body parser
 const app = express();
 
 app.use("/public", express.static(path.join(__dirname, "Static"))); //? static files
-app.use(body.urlencoded({ extended: false })); //? body parser
-app.use(body.json()); //? body parser json data
+app.set("view engine", "ejs"); //? setting the view engine
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Static", "index.html")); //? sending a file to the browser
+app.get("/:userQuery", (req, res) => {
+  res.render("index", {data : {userQuery: req.params.userQuery,
+    searchResults: ["book1", "book2", "book3"], loggedIn: true, username: "John Doe"}}
+   );
 });
-
-app.post("/", (req, res) => {
-//   console.log(req.body);
-  const schema = Joi.object({
-    email: Joi.string().trim().email().required(),
-    password: Joi.string().min(5).max(20).required(),
-  });
-//   schema.validate(req.body, (err, result) => {
-    if (schema.validate(req.body)) {
-      console.log(err);
-      res.send('an error has occured');
-    } else {
-      console.log(result);
-      res.send('successfully posted data');
-    }
-//   }); 
-
-//   console.log(schema.validate(req.body));
-
-
-});
-
-
 
 app.listen(3001);
