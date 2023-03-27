af167cf1d9de533d8a400b718c529f351cfdc5be
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-27 22:49:39 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 
-app.use("/public", express.static(path.join(__dirname, "Static"))); //? static files
-app.set("view engine", "ejs"); //? setting the view engine
+app.use(bodyParser.json());
 
-app.get("/:userQuery", (req, res) => {
-  res.render("index", {data : {userQuery: req.params.userQuery,
-    searchResults: ["book1", "book2", "book3"], loggedIn: true, username: "John Doe"}}
-   );
+
+app.use((req, res, next) => {
+  req.banana = 'banana';
+  next();
+});
+
+app.use('/',(req, res, next) => {
+  // console.log(req.url, req.method); // ? / GET
+  console.log(req.banana); // ? banana
+  next();
+});
+
+// ? /example is the route
+// app.use('/example',(req, res, next) => {
+//   console.log(req.url, req.method);
+//   next();
+// });
+
+app.get("/", (req, res) => {
+  res.send("Middleware");
 });
 
 app.listen(3001);
